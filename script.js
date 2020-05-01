b0e4cc926fbbd06adf5f8021bc7e2ac89ab51035
Your Name <you@example.com>
2020-05-01 12:43:22 +0100

    modifier supprimer

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const table = document.querySelector('table')
 const boutonValider = document.querySelector('#btn')
 const boutonMettreAjour = document.querySelector('#maj')
 const tbody = document.querySelector('#tbody');
+const form = document.querySelector('.ui');
+const boutonreinitialiser = document.querySelector('#reinitialiser')
 const users = []
 
 boutonMettreAjour.style.display = 'none'
+boutonreinitialiser.style.display = 'none'
 
 // checkbox area
 
@@ -24,9 +27,9 @@ checkbox.addEventListener('click', function () {
 boutonValider.addEventListener('click', function(e){
     
         e.preventDefault();
+
         // value recovery
-       
-        
+          
         const firstName = document.querySelector('#firstName')
         const name = document.querySelector('#name')
         const email = document.querySelector('#email')
@@ -35,7 +38,7 @@ boutonValider.addEventListener('click', function(e){
         const number = document.querySelector('#number');
         const state = document.querySelector('#state');
         const country = document.querySelector('#country');
-        tbody.textContent = ' ';
+        
 
         //error handling
 
@@ -118,6 +121,7 @@ boutonValider.addEventListener('click', function(e){
             // Ajouter des elements dans le tableau
 
             const AjouterUnEmployer = ()=>{
+                tbody.textContent = ' ';
                 for (let i = 0; i < users.length; i++) {
                     const htmlSTRING = 
                                       `<tr id="row-${users[i].id}">
@@ -130,30 +134,108 @@ boutonValider.addEventListener('click', function(e){
                                         <td>${users[i].number}</td>
                                         <td>${users[i].state}</td>
                                         <td>${users[i].country}</td>
-                                        <td><button type='button'>modifier</button><input type='hidden' value="${i}"></td>
-                                        <td><button id="btn-${users[i].id}" type='button'>supprimer</button><input type='hidden' value="${i}"></td></tr>`;
+                                        <td><button type='button' class="bt-${users[i].id}"><i class="fas fa-edit"></i></button><input type='hidden' value="${i}"></td>
+                                        <td><button id="btn-${users[i].id}" type='button'><i class="fas fa-trash-alt"></i></button><input type='hidden' value="${i}"></td></tr>`;
 
                             tbody.insertAdjacentHTML('beforeend', htmlSTRING);
+
+                            // bouton supprimer
+
+                            const btnDelete = document.querySelector(`#btn-${users[i].id}`);
+                            const employeID = users[i].id;
+                            btnDelete.addEventListener('click', function() {
+
+                                // affichage d'un alert
+
+                                if (confirm(
+                                    `Etes-vous sûr de supprimer l'employé ${monObjet.firstName}  ${monObjet.name}`
+                                  )){
+                                        const tr = document.querySelector(`#row-${users[i].id}`);
+                                        console.log('TBODY : ', tbody);
+                                        console.log('TR : ', tr);
+                                        tbody.removeChild(tr);
+                                        for (let i = 0; i < users.length; i++) {
+                                            if (users[i].id === employeID) {
+                                                users.splice(i, 1);
+                                            }
+                                        }         
+                                }
+                                
+                            });
+
+                            // btn Modifier
+
+                            const btnModifier = document.querySelector(`.bt-${users[i].id}`)
+                            console.log(btnModifier);
+                            
+                            btnModifier.addEventListener('click', function () {
+                                firstName.value =  monObjet.firstName;
+                                name.value = monObjet.name;
+                                email.value = monObjet.email;
+                                old.value = monObjet.old;
+                                post.value = monObjet.post;
+                                number.value = monObjet.number;
+                                state.value = monObjet.state;
+                                country.value = monObjet.country;
+                                checkbox.checked = true;
+                                boutonValider.style.display = 'none';
+                                boutonMettreAjour.style.display = 'inline';
+                            })
+
+                            // checkbox
+            
+                            checkbox.addEventListener('click', function () {
+                                if (checkbox.checked) {
+                                    boutonMettreAjour.style.display = 'inline';
+                                    boutonValider.style.display = 'none'
+                                }else{
+                                    boutonMettreAjour.style.display = 'none';
+                                }
+                            })
                 }
             }
 
             AjouterUnEmployer()
-            
-            // reinitialisation de zone de saisie
 
-            function reinitialisation() {
-                firstName.value ='';
-                name.value ='';
-                email.value ='';
-                old.value ='';
-                post.value = '';
-                number.value ='';
-                state.value ='';
-                country.value ='';
-                checkbox.checked = false;
-                boutonValider.style.display = 'none';
-            }
+            form.reset();
 
-            reinitialisation()
+                // button mettre a jour
+
+                boutonMettreAjour.addEventListener('click', function (e) {
+                    e.preventDefault()
+
+                    let index= users.findIndex(a => a.id === nombreAleatoir())
+                    users.splice(index, 1, {
+                             id: nombreAleatoir(),
+                        firstName: firstName.value,
+                        name: name.value,
+                        email: email.value,
+                        old: old.value,
+                        post: post.value,
+                        number: number.value,
+                        state: state.value,
+                        country: country.value
+                    })
+                    console.log(index);
+
+                    AjouterUnEmployer()
+
+                    // checkbox 
+
+                    boutonMettreAjour.style.display = 'none';
+
+                    checkbox.addEventListener('click', function () {
+                        if (checkbox.checked) {
+                            boutonMettreAjour.style.display = 'none';
+                            boutonValider.style.display = 'inline';    
+                        }else{
+                            boutonValider.style.display = 'none';
+                        }
+                    })
+                    boutonreinitialiser.style.display = 'inline';
+                    boutonreinitialiser.addEventListener('click', function () {
+                        boutonreinitialiser.style.display = 'none';
+                    })
+                })
         }   
 })
